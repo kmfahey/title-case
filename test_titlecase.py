24d2b1716cf536982a5ac404ebe16354665f51d9
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-03-23 14:23:02 -0700

    added tests for corner cases

--- test_titlecase.py
+++ test_titlecase.py
@@ -7,21 +7,53 @@ from titlecase import title_case
 
 def test_simple_case():
     title = "a tramp's wallet stored by an english goldsmith during his wanderings in germany and france"
-    title_titlecased = title_case(title)
-    assert title_titlecased == "A Tramp's Wallet Stored by an English Goldsmith During His Wanderings in Germany and France"
+    titlecased = title_case(title)
+    assert titlecased == "A Tramp's Wallet Stored by an English Goldsmith During His Wanderings in Germany and France"
 
 def test_begins_with_punct():
     title = "...and it comes out here"
-    title_titlecased = title_case(title)
-    assert title_titlecased == "...And It Comes out Here"
+    titlecased = title_case(title)
+    assert titlecased == "...And It Comes out Here"
 
 def test_ends_with_punct():
     title = "the delmonico cook book: how to buy food, how to cook it, and how to serve it."
-    title_titlecased = title_case(title)
-    assert title_titlecased == "The Delmonico Cook Book: How to Buy Food, How to Cook It, and How to Serve It."
+    titlecased = title_case(title)
+    assert titlecased == "The Delmonico Cook Book: How to Buy Food, How to Cook It, and How to Serve It."
 
 def test_has_period_delimited_acronym():
     title = "s.o.s. aphrodite!"
-    title_titlecased = title_case(title)
-    assert title_titlecased == "S.O.S. Aphrodite!"
+    titlecased = title_case(title)
+    assert titlecased == "S.O.S. Aphrodite!"
+
+def test_has_multi_word_preposition_general_case():
+    title = "in and out of rebel prisons"
+    titlecased = title_case(title)
+    assert titlecased == "In and out of Rebel Prisons"
+
+def test_has_multi_word_preposition_at_beginning():
+    title = "out of the hurly-burly; or, life in an odd corner"
+    titlecased = title_case(title)
+    assert titlecased == "Out of the Hurly-Burly; or, Life in an Odd Corner"
+
+def test_has_multi_word_preposition_at_end():
+    title = "entering the city with the horse i got off of"
+    titlecased = title_case(title)
+    assert titlecased == "Entering the City With the Horse I Got off Of"
+
+def test_has_accented_character_general_case():
+    title = "pascal's pensées"
+    titlecased = title_case(title)
+    assert titlecased == "Pascal's Pensées"
+
+def test_has_accented_character_at_word_start():
+    title = "pascal's pensées"
+    titlecased = title_case(title)
+    assert titlecased == "Pascal's Pensées"
+
+def test_has_accented_character_at_word_start():
+    title = "on conducting (Üeber das dirigiren): a treatise on style in the execution of classical music"
+    titlecased = title_case(title)
+    assert titlecased == "On Conducting (Üeber Das Dirigiren): a Treatise on Style in the Execution of Classical Music"
+
+
 
